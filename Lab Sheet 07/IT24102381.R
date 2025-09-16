ef550caa3778d75ff4e6e06307f0b2b5d2b7b2c8
IT24102381 <it24102381@my.sliit.lk>
2025-09-16 22:38:54 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102381.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\yuthi\\Desktop\\IT24102381")
+
+#01
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#02
+pexp(2, rate = 1/3)
+
+
+#03
+
+#i
+1 - pnorm(130, 100, 15)
+
+##ii
+qnorm(0.95, 100, 15)
